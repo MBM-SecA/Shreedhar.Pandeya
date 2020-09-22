be774ee8278a55bcc52a7ef130f7e2c7edd870d1
fallen700 <shreedharpandey000@gmail.com>
2020-09-22 11:46:44 +0545

    write into a file

--- CSharpBasics/FileIO.cs
+++ CSharpBasics/FileIO.cs
@@ -5,12 +5,20 @@ public class FileIO
 {
     public void LearnFileHandling()
     {
-        string filename = (@"C:\Users\User\Desktop\C# github repo\Readme.txt");
+    //     string filename = (@"C:\Users\User\Desktop\C# github repo\Readme.txt");
+    //     if(File.Exists(filename))
+    //    { string FIleCOntent = File.ReadAllText(@"C:\Users\User\Desktop\C# github repo\Readme.txt");
+    //     Console.WriteLine(FIleCOntent);
+
+    //    }
+     string filename = "file.txt";
         if(File.Exists(filename))
-       { string FIleCOntent = File.ReadAllText(@"C:\Users\User\Desktop\C# github repo\Readme.txt");
+       { string FIleCOntent = File.ReadAllText(filename);
+       
         Console.WriteLine(FIleCOntent);
 
        }
+       File.WriteAllText(filename, "This is just a text file");
 
     }
 }
