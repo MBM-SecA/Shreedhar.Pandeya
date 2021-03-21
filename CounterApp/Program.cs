0007f9106f56d0973b4c29e6ed7cdb4628251760
Shreedhar73 <shreedharpandey000@gmail.com>
2021-03-21 16:48:27 +0545

    counter

---
+++ CounterApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+namespace CounterApp
+{
+    class Program
+    {
+        static void Main(string[] argument)
+        {
+            int limit = int.MaxValue;
+            if (argument.Length>0)
+            {
+                limit = int.Parse(argument[0]);
+
+
+
+            }
+            int counter = 1;
+            while(counter < limit)
+            {
+                Console.WriteLine(counter);
+                counter++;
+                await.Task.Delay(1000);
+
+            }
+        }
+    }
+}
